69c2037aa27d85769dd62904654ed463082a4d79
Ahalekin <118134379+Ahalekin@users.noreply.github.com>
2026-05-14 23:31:31 -0300

    Fix typo in production base path

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ const emitVersionJson = () => ({
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Certificadora_comun_html_css/" : "/",
+  base: mode === "production" ? "/Certificadora_comum_html_css/" : "/",
   define: {
     __BUILD_ID__: JSON.stringify(BUILD_ID),
   },
